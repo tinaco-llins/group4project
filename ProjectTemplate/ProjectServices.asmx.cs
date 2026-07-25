c25fa542fd03d74ddb685168871b0abc0dd369de
Your Name <you@example.com>
2026-07-25 03:35:14 -0700

    Add feedback form with required problem and solution fields

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -19,16 +19,17 @@ namespace ProjectTemplate
 		////////////////////////////////////////////////////////////////////////
 		///replace the values of these variables with your database credentials
 		////////////////////////////////////////////////////////////////////////
-		private string dbID = "cis440template";
-		private string dbPass = "!!Cis440";
-		private string dbName = "cis440template";
+		private string dbID = "cis440sum26team4";
+		private string dbPass = "cis440sum26team4";
+		private string dbName = "cis440sum26team4";
 		////////////////////////////////////////////////////////////////////////
-		
+
 		////////////////////////////////////////////////////////////////////////
 		///call this method anywhere that you need the connection string!
 		////////////////////////////////////////////////////////////////////////
-		private string getConString() {
-			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
+		private string getConString()
+		{
+			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
 		}
 		////////////////////////////////////////////////////////////////////////
 
@@ -59,8 +60,57 @@ namespace ProjectTemplate
 			}
 			catch (Exception e)
 			{
-				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
+				return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
+			}
+		}
+
+		/////Story 4: As an employee, I'd like to be prompted to enter both a problem and suggested solution so that my feedback will be actionable.//////
+		[WebMethod(EnableSession = true)]
+
+		[System.Web.Script.Services.ScriptMethod(ResponseFormat = System.Web.Script.Services.ResponseFormat.Json)]
+
+		public string SubmitFeedback(string employeeName, string problem, string suggestedSolution)
+		{
+			if (string.IsNullOrWhiteSpace(problem) || string.IsNullOrWhiteSpace(suggestedSolution))
+			{
+				return "{\"success\": false, \"message\": \"Both a problem and a suggested solution are required.\"}";
+			}
+
+			try
+			{
+				using (MySqlConnection con = new MySqlConnection(getConString()))
+				{
+					con.Open();
+
+					string sql = "INSERT INTO actionable_feedback (employeeName, problem, suggestedSolution) " + "VALUES (@employeeName, @problem, @suggestedSolution)";
+
+					using (MySqlCommand cmd = new MySqlCommand(sql, con))
+					{
+						object employeeNameValue;
+						if (string.IsNullOrWhiteSpace(employeeName))
+						{
+							employeeNameValue = DBNull.Value;
+						}
+						else
+						{
+							employeeNameValue = employeeName.Trim();
+						}
+						cmd.Parameters.AddWithValue("@employeeName", employeeNameValue);
+
+						cmd.Parameters.AddWithValue("@problem", problem.Trim());
+						cmd.Parameters.AddWithValue("@suggestedSolution", suggestedSolution.Trim());
+						cmd.ExecuteNonQuery();
+					}
+				}
+				return "{\"success\": true, \"message\": \"Feedback saved.\"}";
+			}
+			catch (Exception e)
+			{
+				return "{\"success\": false, \"message\": \"" + e.Message.Replace("\"", "'") + "\"}";
+
 			}
 		}
 	}
 }
+
+
